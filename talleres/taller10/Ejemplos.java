9f7378d60c7e1bbcdf34d3ef79f5c166f7949bdc
eafit-201710076010 <30275940+eafit-201710076010@users.noreply.github.com>
2017-10-16 15:46:18 -0500

    Add files via upload

--- talleres/taller10/Ejemplos.java
+++ talleres/taller10/Ejemplos.java
@@ -2,28 +2,34 @@ public class Ejemplos
 {
     public static void main (String [] args){
         Arbol arGen=new Arbol("Wilkenson");
+        //padres
         Nodo madre =new Nodo("Joaquina");
         arGen.getRaiz().left=madre;
         Nodo padre=new Nodo("Sufranio");
         arGen.getRaiz().right=padre;
+        //abuelos maternos 
         Nodo abam=new Nodo("Eustaquia");
         madre.left=abam;
         Nodo abom=new Nodo("Eustaquio");
         madre.right=abom;
+        //abuelos paternos
         Nodo abap=new Nodo("Piolina");
         padre.left=abap;
         Nodo abop=new Nodo("Piolín");
         padre.right=abop;
+        //bisabuelos maternos
         Nodo babam= new Nodo("Florinda");
         abam.left=babam;
         Nodo babom= new Nodo("Jovín");
         abom.right=babom;
+        //bisabuelos paternos
         Nodo babap= new Nodo("Wilberta");
         abap.left=babap;
         Nodo babop= new Nodo("Usnavy");
         abop.right=babop;
-        Arbol.dibujarArbol(arGen);
+        
         arGen.impInorden();
-
+        System.out.println("\nDatos para el gráfico\n");
+        Arbol.dibujarArbol(arGen);
     }
 }
