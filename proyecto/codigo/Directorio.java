a83b631e819e4234030a2b234a6b85346785b0a2
lcrodriguz <30600715+lcrodriguz@users.noreply.github.com>
2017-10-27 15:06:04 -0500

    Avance

--- proyecto/codigo/Directorio.java
+++ proyecto/codigo/Directorio.java
@@ -1,61 +1,84 @@
-
-/**
- * Arbol rojonegro
- * 
- * @author Manu 
- * @version number 1
- */
 import java.util.*;
 import java.io.*;
 import java.util.regex.Pattern;
-public class Directorio
-{
+
+public class Directorio{
+
     String direccion;
     TreeMap<String,String> Nombres = new TreeMap<String,String>();
     TreeMap<String,String>  Tamaños= new TreeMap<String,String>();
     TreeMap<String,String> Dueños = new TreeMap<String,String>();
+
     File archivo=null;
-        FileReader fr=null;
-        BufferedReader br=null;
+    FileReader fr=null;
+    BufferedReader br=null;
+
     public Directorio(){
-        String a="hola";
-         int b= a.hashCode();}
-       public void leerArchivo(){ 
-           String a="Manuela";
-           String [] j= a.split("a");
-        try {
+
+    }
+
+    public void leer(){
+
+        String linea;
+        String siglinea;
+        String tmp="";
+
+        String rara2 = Pattern.quote("└");
+        String rara = Pattern.quote("├");
+
+        String [] spline = null;
+        String [] spsline = null;
+        try{ 
+
             archivo = new File ("archivo.txt");
             fr = new FileReader (archivo);
             br = new BufferedReader(fr);
-            String siglinea;
-            String linea;
-            String tmp="";
-            String separador = Pattern.quote("│");
-            String espacio = Pattern.quote(" ");
+
             linea=br.readLine();
             linea=br.readLine();
             siglinea=br.readLine();
-          while(linea!=null|| siglinea!=null)
 
-            { 
-               
-               String[] pline = linea.split(separador);
-                int num=pline.length;
-               String[] psigline = siglinea.split(separador);
-                int numsig=psigline.length;
-                String [] espaciossig= siglinea.split(espacio);
-                String [] espacios= linea.split(espacio);
-            if(num+1==numsig){
-            Nombres.put(espaciossig[espaciossig.length-1],espacios[espacios.length-1]);
-            } else{
-            linea=tmp;
-            continue;
-            }
+            while(linea!=null || siglinea!=null){
+
+                if(linea.contains("├")){
+                    spline = linea.split(rara);
+                }
+                if(linea.contains("└")){
+                    spline = linea.split(rara2);
+                }
+                if(siglinea.contains("├")){
+                    spsline = siglinea.split(rara);
+                }
+                if(siglinea.contains("└")){
+                    spsline = siglinea.split(rara2);
+                }
+
+                int espline = spline[0].length();
+                int espsline = spsline[0].length();
+
+                int ecuacion = 2 * espline;
+                
+                if(ecuacion == espsline){
+                    
+                    Nombres.put(spsline[spsline.length-1] , spline[spline.length-1]);
+                    
+                    linea=siglinea;
+                    siglinea=br.readLine();
+                    continue;
+                }
+                
+                if(espline > espsline){
+                    Nombres.put(spsline[spsline.length-1] , spline[spline.length-1]);
+                }
+                
+                if(espline < espsline){
+                    Nombres.put(spsline[spsline.length-1] , spline[spline.length-1]);
+                }
                 //Nombres.put(linea,siglinea);  
-                tmp=siglinea;
-                 siglinea=br.readLine();
-            }}   
-
+                tmp=linea;
+                siglinea=br.readLine();
+            }
+        }   
         catch(Exception e){
             e.printStackTrace();
         }finally{
@@ -69,13 +92,14 @@ public class Directorio
             }
         }
     }
+
     public void recorrer(){
-    
+
     }
 
     public void buscar(){
-         System.out.println(Nombres.get("asus-keyboard-backlight-down"));
-        direccion="proyecto/plantilas/mauriciotoro";
+        System.out.println(Nombres.get("asus-keyboard-backlight-down"));
+        direccion="proyecto";
 
         System.out.println(Nombres.tailMap(direccion));
     }
